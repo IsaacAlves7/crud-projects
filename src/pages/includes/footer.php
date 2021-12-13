187c560bf70b275b1a5154588223ff55e7d69387
Isaac Alves Pinheiro <61624336+IsaacAlves7@users.noreply.github.com>
2021-12-13 01:10:04 -0300

    Delete src/pages directory

--- src/pages/includes/footer.php
+++
@@ -1,7 +0,0 @@
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script>
-        M.AutoInit();
-    </script>
-    </body>
-</html>
